5593ef25567bfcd6c78363590b334bb3b8fbb0cd
Jose <jose.amadormora@ucr.ac.cr>
2024-09-12 03:28:16 -0600

    Laboratorio 5 terminado

---
+++ backend-lab-C10421/backend-lab-C10421/Controllers/PaisesController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using backend_lab_C10421.Models;
+using backend_lab.Handlers;
+
+namespace backend_lab_C10421.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PaisesController : ControllerBase
+    {
+        private readonly PaisesHandler _paisesHandler;
+
+        public PaisesController()
+        {
+            _paisesHandler = new PaisesHandler();
+        }
+
+        [HttpGet]
+        public List<PaisModel> Get()
+        {
+            var paises = _paisesHandler.ObtenerPaises();
+            return paises;
+        }
+    }
+}
