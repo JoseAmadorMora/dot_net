c34abd4514bac9ec8a00f849fdd4a08eb7741ca9
Jose <jose.amadormora@ucr.ac.cr>
2024-11-12 19:44:39 -0600

    se añaden esperas clickable

--- backend-lab-C10421/UIAutomationTests/Selenium.cs
+++ backend-lab-C10421/UIAutomationTests/Selenium.cs
@@ -1,19 +1,22 @@
 ﻿using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
 using OpenQA.Selenium.Interactions;
+using OpenQA.Selenium.Support.UI;
 
 namespace UIAutomationTests
 { 
-    public class Selenium
+public class Selenium
     {
         IWebDriver _driver;
         Actions _actions;
+        WebDriverWait _wait;
 
-        [SetUp]   
+        [SetUp]
         public void Setup()
         {
             _driver = new ChromeDriver();
             _actions = new Actions(_driver);
+            _wait = new WebDriverWait(_driver, TimeSpan.FromSeconds(10));
         }
 
         [Test]
@@ -25,20 +28,22 @@ namespace UIAutomationTests
             _driver.Manage().Window.Maximize();
             //Navega a la pagina que se necesita probar
             _driver.Navigate().GoToUrl(URL);
-            int cantidadPaises = _driver.FindElements(By.Id("lista-paises")).Count;
-            var botonAgregarPais = _driver.FindElement(By.Id("boton-ir-a-agregar-pais"));
+            int cantidadPaisesTabla = _driver.FindElements(By.CssSelector("#lista-paises tbody tr")).Count;
+
+            var botonAgregarPais = _wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("boton-ir-a-agregar-pais")));
             _actions.MoveToElement(botonAgregarPais).Click().Perform();
 
             var inputNombrePais = _driver.FindElement(By.Id("name"));
             var selectContinente = _driver.FindElement(By.Id("continente"));
             var inputIdioma = _driver.FindElement(By.Id("idioma"));
-
             inputNombrePais.SendKeys("Mexico");
             selectContinente.SendKeys("América");
             inputIdioma.SendKeys("Español");
-            _driver.FindElement(By.Id("btn-guardar-pais")).Click();
 
-            Assert.Greater(_driver.FindElements(By.Id("lista-paises")).Count, cantidadPaises);
+            var botonGuardarPais = _wait.Until(SeleniumExtras.WaitHelpers.ExpectedConditions.ElementToBeClickable(By.Id("btn-guardar-pais")));
+            botonGuardarPais.Click();
+
+            Assert.Greater(_driver.FindElements(By.CssSelector("#lista-paises tbody tr")).Count, cantidadPaisesTabla);
         }
     }
 }
